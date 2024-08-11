bec27b846b1b6d2ff0794d1e776fc65c26303c34
Mehul721 <jmehul721@gmail.com>
2024-08-11 18:35:31 +0530

    Initial Commit

---
+++ StormSense/StormSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormSenseApp.swift
+//  StormSense
+//
+//  Created by Mehul Jain on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StormSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
