ac67e28d46628ffb3f55a7bcf14a3c6759f93419
Mehul721 <jmehul721@gmail.com>
2024-08-19 23:56:59 +0530

    added overlay for inner shadow to bottomsheet part-1

--- StormSense/Views/Details/ForecastView.swift
+++ StormSense/Views/Details/ForecastView.swift
@@ -12,23 +12,32 @@ struct ForecastView: View {
     var body: some View {
         ScrollView {
             
-        } 
+        }
         .backgroundblur(radius: 25,opaque:true)
         .clipShape(RoundedRectangle(cornerRadius: 44))
         .overlay{
-            Divider()
-                .blendMode(.overlay)
-                .background(Color.bottomSheetBorderTop)
-                .frame(maxHeight: .infinity, alignment: .top)
-                .clipShape(RoundedRectangle(cornerRadius: 44))
-        }
-        .overlay{
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(.black.opacity(0.3))
-                    .frame(width: 48, height: 5)
-                    .frame(height:20)
-                    .frame(maxHeight: .infinity, alignment: .top)
-            }
+            RoundedRectangle(cornerRadius: 44)
+                .stroke(.white,lineWidth: 1)
+                .offset(y:1)
+                .blur(radius: 0)
+                .mask{
+                    RoundedRectangle(cornerRadius: 44)
+                }
+            
+                .overlay{
+                    Divider()
+                        .blendMode(.overlay)
+                        .background(Color.bottomSheetBorderTop)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .clipShape(RoundedRectangle(cornerRadius: 44))
+                }
+                .overlay{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.black.opacity(0.3))
+                        .frame(width: 48, height: 5)
+                        .frame(height:20)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                }
         }
     }
     
@@ -38,4 +47,4 @@ struct ForecastView: View {
                 .preferredColorScheme(.dark)
         }
     }
-
+}
