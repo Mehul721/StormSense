c240b335ceb9a9e4a04c457cbbd4915435fb3496
Mehul721 <jmehul721@gmail.com>
2024-08-21 23:56:23 +0530

    Added parallax effect part-1

--- StormSense/Views/Main/HomeView.swift
+++ StormSense/Views/Main/HomeView.swift
@@ -15,58 +15,69 @@ enum BottomSheetPosition: CGFloat, CaseIterable {
 
 struct HomeView: View {
     @State var bottomSheetPosition: BottomSheetPosition = .middle
-
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    
+    
+    var bottomSheetTranslationProrated:CGFloat{
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue)/(BottomSheetPosition.top.rawValue-BottomSheetPosition.middle.rawValue)
+    }
+    
     var body: some View {
         NavigationView {
-            ZStack {
-                Color("background")
-                    .ignoresSafeArea()
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                Image("House")
-                    .frame(maxWidth: .infinity, alignment: .top)
-                    .padding(.top, 137)
-
-                VStack(spacing: -10) {
-                    Text("Bengaluru")
-                        .font(.largeTitle)
-                    VStack {
-                        Text(attributedString)
-
-                        Text("H:24°  L:18°")
-                            .font(.title3.weight(.semibold))
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height +
+                geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                ZStack {
+                    Color("background")
+                        .ignoresSafeArea()
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                    Image("House")
+                        .frame(maxWidth: .infinity, alignment: .top)
+                        .padding(.top, 137)
+                    
+                    VStack(spacing: -10) {
+                        Text("Bengaluru")
+                            .font(.largeTitle)
+                        VStack {
+                            Text(attributedString)
+                            
+                            Text("H:24°  L:18°")
+                                .font(.title3.weight(.semibold))
+                        }
+                        Spacer()
                     }
-                    Spacer()
+                    .padding(.top, 51)
+                    BottomSheetView(position:$bottomSheetPosition){
+                        Text(bottomSheetTranslationProrated.formatted())
+                    }
+                    
+                    .navigationBarHidden(true)
                 }
-                .padding(.top, 51)
-                BottomSheetView(position:$bottomSheetPosition){
-                   
-                }
-                TabBar(action: {})
             }
         }
-    }
-
-    private var attributedString: AttributedString {
-        var string = AttributedString("19°\nMostly Clear")
-
-        if let temp = string.range(of: "19°") {
-            string[temp].font = .system(size: 96, weight: .thin)
-            string[temp].foregroundColor = .primary
+        
+        var attributedString: AttributedString {
+            var string = AttributedString("19°\nMostly Clear")
+            
+            if let temp = string.range(of: "19°") {
+                string[temp].font = .system(size: 96, weight: .thin)
+                string[temp].foregroundColor = .primary
+            }
+            
+            if let weather = string.range(of: "Mostly Clear") {
+                string[weather].font = .title3.weight(.semibold)
+                string[weather].foregroundColor = .secondary
+            }
+            return string
         }
-
-        if let weather = string.range(of: "Mostly Clear") {
-            string[weather].font = .title3.weight(.semibold)
-            string[weather].foregroundColor = .secondary
-        }
-        return string
     }
-}
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-            .preferredColorScheme(.dark)
+    
+    struct HomeView_Previews: PreviewProvider {
+        static var previews: some View {
+            HomeView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
