c5abfa1dae9b6f41615e275a370215ead38eddab
Mehul721 <jmehul721@gmail.com>
2024-08-31 23:48:48 +0530

    Completed design of forecast card

--- StormSense/Views/Components/ForecastCard.swift
+++ StormSense/Views/Components/ForecastCard.swift
@@ -7,31 +7,58 @@
 
 import SwiftUI
 
-struct InnerShadow: ViewModifier {
-    var color: Color
-    var radius: CGFloat
-    var x: CGFloat
-    var y: CGFloat
-
-    func body(content: Content) -> some View {
-        content
-            .overlay(
-                content
-                    .clipShape(RoundedRectangle(cornerRadius: 30))
-                    .shadow(color: color, radius: radius, x: x, y: y)
-                    .clipShape(
-                        RoundedRectangle(cornerRadius: 30)
-                            .inset(by: -radius)
-                    )
-                    .mask(
-                        RoundedRectangle(cornerRadius: 30)
-                    )
-            )
+struct ForecastCard: View {
+    var forecast: Forecast
+    var forecastPeriod:ForecastPeriod
+    var isActive:Bool{
+        if forecastPeriod == ForecastPeriod.hourly{
+            let isThisHour = Calendar.current.isDate(.now,equalTo:forecast.date,toGranularity: .hour)
+            return isThisHour
+        }else{
+            let isToday = Calendar.current.isDate(.now,equalTo:forecast.date,toGranularity: .day)
+            return isToday
+        }
+    }
+    
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 30)
+                .fill(Color.forecastCardBackground.opacity(isActive ? 1:0.2))
+                .frame(width:60,height:146)
+                .shadow(color:.background.opacity(0.25),radius: 10,x:5,y:4)
+                .overlay{
+                    RoundedRectangle(cornerRadius: 30)
+                        .strokeBorder(.white.opacity(isActive ? 0.5:0.2))
+                        .blendMode(.overlay)
+                }
+                .innerShadow(shape: RoundedRectangle(cornerRadius:30), color: .white.opacity(0.25),lineWidth: 1,OffsetX: 1,OffsetY: 1,blur:0,blendMode: .overlay)
+            
+            VStack(spacing:16){
+                Text(forecast.date,format:forecastPeriod ==
+                     ForecastPeriod.hourly ? .dateTime.hour():
+                        .dateTime.weekday())
+                .font(.subheadline.weight(.semibold))
+                VStack(spacing:-4){
+                    Image("\(forecast.icon)small")
+                    Text(forecast.probability,format:.percent)
+                        .font(.footnote.weight(.semibold))
+                        .foregroundStyle(Color.probabilityText)
+                        .opacity(forecast.probability > 0 ? 1 : 0)
+                    
+                }
+                .frame(height:42)
+                
+                Text("\(forecast.temperature)°")
+                    .font(.title3)
+            }
+            .padding(.horizontal.8)
+            .padding(.vertical,16)
+            .frame(width:60,height:146)
+            
+        }
     }
 }
 
-extension View {
-    func innerShadow(color: Color, radius: CGFloat, x: CGFloat, y: CGFloat) -> some View {
-        self.modifier(InnerShadow(color: color, radius: radius, x: x, y: y))
-    }
+#Preview {
+    ForecastCard(forecast: Forecast.hourly[0], forecastPeriod: .hourly)
 }
