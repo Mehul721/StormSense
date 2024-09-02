128aa35acc5a2d1588215fdf04038ccb61149a1e
Mehul721 <jmehul721@gmail.com>
2024-09-02 23:50:59 +0530

    Added the custom navigation bar

---
+++ StormSense/Views/Details/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  StormSense
+//
+//  Created by Mehul Jain on 02/09/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
